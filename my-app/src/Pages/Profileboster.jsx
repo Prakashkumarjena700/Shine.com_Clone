fd9f014480c5ba7879b501a386a22664fce52dda
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2022-10-01 01:17:24 +0530

    added Profileboster

---
+++ my-app/src/Pages/Profileboster.jsx
@@ -0,0 +1,155 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/SecondaryNavbar'
+import './JobseekingAssistance.css'
+
+export default function Profileboster() {
+    return (
+        <div>
+            <Navbar />
+            <div className='speace' ></div>
+
+            <div className='profilebosterfirstFlex' >
+                <div>
+                    <h1>Profile Booster</h1>
+                    <p> <img width='15px' style={{ display: 'inline-block' }} src="https://www.pngall.com/wp-content/uploads/9/Golden-Star-PNG-Image-File.png" alt="" /> 4 (Bought by 179 people)</p>
+                    <p>With this service, you can improve your visibility to recruiters and get hired faster at Shine.  Profile Booster is a 15-day validity bouquet offering that helps you improve your profile and get better visibility with recruiters. ...</p>
+                </div>
+                <div>
+                    <img src="https://static1.shine.com/l/m/product_images/1643743291_2188.png" alt="" />
+                </div>
+            </div>
+            <h1 className='firstProfilebosterheading'>Why do you need Profile Booster ?</h1>
+            <div className='secondProfilebosterheading' >
+                <div>
+                    <div className='firstDiv'></div>
+                    <div className='secondDiv' >
+                        <p><strong>Without</strong></p>
+                        <p><strong>With</strong></p>
+                    </div>
+                </div>
+                <div>
+                    <div className='firstDiv'>Highlight your application to recruiter</div>
+                    <div className='secondDiv' >
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                        <img src="https://1.bp.blogspot.com/-1Xhj-_abiPU/XQLEtERwR9I/AAAAAAAAJrk/Qnx4XH-c45UidfWg6aJVJUyzg7XJbxjBQCK4BGAYYCw/s1600/yes%2Bcheck%2B.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div className='firstDiv'>Get Upto 10X attention from recruiters</div>
+                    <div className='secondDiv' >
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                        <img src="https://1.bp.blogspot.com/-1Xhj-_abiPU/XQLEtERwR9I/AAAAAAAAJrk/Qnx4XH-c45UidfWg6aJVJUyzg7XJbxjBQCK4BGAYYCw/s1600/yes%2Bcheck%2B.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div className='firstDiv'>Profile is tagged as Priority applicant</div>
+                    <div className='secondDiv' >
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                        <img src="https://1.bp.blogspot.com/-1Xhj-_abiPU/XQLEtERwR9I/AAAAAAAAJrk/Qnx4XH-c45UidfWg6aJVJUyzg7XJbxjBQCK4BGAYYCw/s1600/yes%2Bcheck%2B.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div className='firstDiv'>Increase your chance of shortlisting</div>
+                    <div className='secondDiv' >
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                        <img src="https://1.bp.blogspot.com/-1Xhj-_abiPU/XQLEtERwR9I/AAAAAAAAJrk/Qnx4XH-c45UidfWg6aJVJUyzg7XJbxjBQCK4BGAYYCw/s1600/yes%2Bcheck%2B.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div className='firstDiv'>Active support from our customer service team</div>
+                    <div className='secondDiv' >
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                        <img src="https://1.bp.blogspot.com/-1Xhj-_abiPU/XQLEtERwR9I/AAAAAAAAJrk/Qnx4XH-c45UidfWg6aJVJUyzg7XJbxjBQCK4BGAYYCw/s1600/yes%2Bcheck%2B.png" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div className='firstDiv'>Guaranteed job</div>
+                    <div className='secondDiv' >
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                        <img src="https://midoriclarkcasino.com/wp-content/uploads/2020/07/reject-icon-29.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className='secondProfileDiv' >
+                <h1>Combo Includes</h1>
+                <button>Cover Letter</button><button>Resume Builder - 15 days</button><button>Featured Profile 15 Days</button><button>Application Highlighter - 15 Days</button>
+            </div>
+            <div className='thirdProfileDiv' >
+                <div>
+                    <img width='20px' src="https://beaconconverters.com/wp-content/uploads/2019/10/icon-bullet-point-300x288.png" alt="" />
+                    <p><strong>₹1999</strong></p>
+                </div>
+                <div className='thirdProfileDivSecond' >
+                    <strong>Total Price ₹1999 ₹2999 <span style={{ color: 'lightgreen' }} >(33% OFF)</span></strong>
+                    <button className='btny' >Buy now</button>
+                    <button className='btnv' >Talk to Expert</button>
+                </div>
+            </div>
+            <p style={{ marginLeft: '60px' }} ><strong>How It Works</strong> &nbsp; &nbsp; FAQs &nbsp; &nbsp; Reviews</p>
+            <div className='forthProfileDiv' >
+                <h1 className='firstProfilebosterheading' >How It Works?</h1>
+                <div>
+                    <div>
+                        <div><img width='30px' src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png" alt="" /></div>
+                        <div>
+                            <strong>Place Order</strong>
+                            <p>Place order and upload your latest</p>
+                            <p>resume</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div><img width='30px' src="https://www.freeiconspng.com/thumbs/resume-icon-png/resume-icon-png-15.png" alt="" /></div>
+                        <div>
+                            <strong>Profile Featured</strong>
+                            <p>Profile Highlighted to recruiters</p>
+                            <p>and marked as featured profile</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div><img width='30px' src="https://www.freeiconspng.com/thumbs/resume-icon-png/resume-icon-png-15.png" alt="" /></div>
+                        <div>
+                            <strong>Application Highlighted</strong>
+                            <p>Apply to jobs & stand out among</p>
+                            <p>other applicants through</p>
+                            <p>highlighting your job application</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div><img width='30px' src="https://www.iconpacks.net/icons/1/free-phone-icon-1-thumb.png" alt="" /></div>
+                        <div>
+                            <strong>Interview calls</strong>
+                            <p>10X more chances to get a call</p>
+                            <p>from recruiter</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='fifthProfileDiv' >
+                <h1 className='firstProfilebosterheading' >FAQs</h1>
+                <details>
+                    <summary><strong>Which all services are included in Profile Booster?</strong></summary>
+                    Profile Booster includes Application Highlighter, Featured Profile, Resume Builder and Cover Letter.
+                </details>
+                <details>
+                    <summary><strong>How can I use the Featured Profile?</strong></summary>
+                    After you purchase Shine Premium or Profile Booster, you will get a welcome call from one of our experts and then your featured profile will be activated for 15 days for Profile Booster, 30 days for Shine Premium Basic and for 60 days for Shine Premium Pro. With featured profile, your profile will be highlighted in recruiter’s search and will be among the first ones to be seen by recruiters every time you appear in a search, for the validity period of featured profile.
+                </details>
+                <details>
+                    <summary><strong>How can I use the Application Highlighter?</strong></summary>
+                    After you purchase Shine Premium or Profile Booster, application highlighter will be automatically activated for 15 days for Profile Booster, 30 days for Shine Premium Basic and for 60 days for Shine Premium Pro. With application highlighter, all the jobs you apply will be brought at the top amongst other applicants and will be highlighted to make you stand out to recruiters, for the validity period of application highlighter.
+                </details>
+                <details>
+                    <summary><strong>How can I use the Cover Letter?</strong></summary>
+                    After you purchase Shine Premium or Profile Booster, you will get a welcome call from one of our experts. We have a team of experts with extensive experience in resume and cover letter writing. The expert will help craft a professional cover letter for you.
+                </details>
+                <details>
+                    <summary><strong>How can I use the Resume Builder?</strong></summary>
+                    After you purchase Shine Premium or Profile Booster, your Resume Builder will be automatically activated for 15 days for Profile Booster, 30 days for Shine Premium Basic and for 60 days for Shine Premium Pro.
+
+                    You will get an option on the order summary page to build your resume using the resume builder. Select this option to go to resume builder page. Once you land on the resume builder page you can select a resume template to build your new awesome resume in just 2 steps and start impressing the employers.
+                </details>
+            </div>
+            <h1 className='firstProfilebosterheading' >Reviews</h1>
+        </div>
+    )
+}
